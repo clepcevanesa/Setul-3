abe955ab408fe0a2d2c7a5a2aef8d087c181e0dd
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-10 18:09:40 +0200

    Add files via upload

---
+++ Ex11/Program.cs
@@ -0,0 +1,44 @@
+﻿//Se da un numar natural n. Se cere sa se afiseze toate numerele prime mai mici sau egale cu n (ciurul lui Eratostene). 
+
+using System;
+
+class CiurulluiEratostene
+{
+    static void Main()
+{
+    Console.Write("Introduceti valoarea lui n: ");
+    int n = int.Parse(Console.ReadLine());
+
+    AfiseazaNumerePrime(n);
+}
+
+static void AfiseazaNumerePrime(int n)
+{
+    bool[] estePrim = new bool[n + 1];
+
+    for (int i = 2; i <= n; i++)
+    {
+        estePrim[i] = true;
+    }
+
+    for (int p = 2; p * p <= n; p++)
+    {
+        if (estePrim[p] == true)
+        {
+            for (int i = p * p; i <= n; i += p)
+            {
+                estePrim[i] = false;
+            }
+        }
+    }
+
+    Console.WriteLine($"Numerele prime mai mici sau egale cu {n} sunt:");
+    for (int i = 2; i <= n; i++)
+    {
+        if (estePrim[i])
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
+}
