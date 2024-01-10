275830f7ade4f280eb4292b5d575d0ab8bcab9c7
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-10 22:13:12 +0200

    Add files via upload

---
+++ Ex28/Program.cs
@@ -0,0 +1,63 @@
+﻿//Quicksort. Sortati un vector folosind metoda QuickSort.
+
+using System;
+
+class QuickSortExample
+{
+    static void Main()
+    {
+        int[] array = { 12, 5, 6, 2, 10, 3, 8, 15, 7 };
+        Console.WriteLine("Array before sorting:");
+        PrintArray(array);
+
+        QuickSort(array, 0, array.Length - 1);
+
+        Console.WriteLine("\nArray after sorting:");
+        PrintArray(array);
+    }
+
+    static void QuickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int partitionIndex = Partition(arr, low, high);
+
+            QuickSort(arr, low, partitionIndex - 1);
+            QuickSort(arr, partitionIndex + 1, high);
+        }
+    }
+
+    static int Partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                Swap(arr, i, j);
+            }
+        }
+
+        Swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    static void Swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
